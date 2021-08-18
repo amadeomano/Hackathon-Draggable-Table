62ca19e775ec34db3bc860957c3b1797de91dfd6
Amadeo Manouchehri <amadeo.manouchehri@gmail.com>
2021-08-18 15:00:29 +0200

    Install dependencies + remove fixed columns and headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import "./styles.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import withScrolling from "react-dnd-scrolling";
 import Table from "./highlight-ui/table/Table";
 import { columns, data } from "./data";
+
+const ScrollWrapper = withScrolling("div");
 
 export default function App() {
   return (
     <div className="App">
-      <Table
-        columns={columns}
-        data={data}
-        theme={undefined}
-        fixedColumnCount={1}
-        fixedHeader
-      />
+      <DndProvider backend={HTML5Backend}>
+        <ScrollWrapper>
+          <Table
+            columns={columns}
+            data={data}
+            theme={undefined}
+            fixedColumnCount={undefined}
+            fixedHeader={undefined}
+          />
+        </ScrollWrapper>
+      </DndProvider>
     </div>
   );
 }
